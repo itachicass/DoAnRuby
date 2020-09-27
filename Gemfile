f52b0fb451cb5f3460348ec0143a848e8d8428bc
itachicass <yumi.kudo.1006@gmail.com>
2020-09-27 17:39:18 +0700

    them admin so bo

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'flex-slider-rails'
 gem 'bcrypt'
 gem "devise", "~> 4.1"
 gem 'pagy'
+gem 'activeadmin'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_101550) do
+ActiveRecord::Schema.define(version: 2020_09_27_101755) do
+
+  create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "tensach"
@@ -20,8 +46,8 @@ ActiveRecord::Schema.define(version: 2020_09_09_101550) do
     t.integer "dongia"
     t.integer "soluong"
     t.string "hinhanh"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", default: -> { "current_timestamp()" }, null: false
+    t.datetime "updated_at", default: -> { "current_timestamp()" }, null: false
     t.text "mota", null: false
   end
 
@@ -76,7 +102,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_101550) do
     t.string "name", null: false
     t.string "gender", null: false
     t.string "address", null: false
-    t.datetime "date_of_birth", null: false
+    t.date "date_of_birth", null: false
     t.string "phone_number", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
